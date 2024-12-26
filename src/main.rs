b0bec557cb9f7dc9ef6683d4c914415f351702a1
tushuguang <tushuguang@sz-sgd.com>
2024-12-26 12:37:19 +0800

    changes

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,20 @@ fn main() {
         let key = console::Term::stdout().read_key().unwrap();
 
         if key == console::Key::ArrowUp     { main_field.move_cursor(-1, 0); }
-        if key == console::Key::ArrowDown   { main_field.move_cursor(1, 0); }
-        if key == console::Key::ArrowLeft   { main_field.move_cursor(0, -1); }
-        if key == console::Key::ArrowRight  { main_field.move_cursor(0, 1); }
+        if key == console::Key::ArrowDown   { main_field.move_cursor(1,  0); }
+        if key == console::Key::ArrowLeft   { main_field.move_cursor(0,  1); }
+        if key == console::Key::ArrowRight  { main_field.move_cursor(0,  1); }
 
-        if key == console::Key::Char(' ') && !main_field.open() {
-            println!("\n--- Oops, 踩雷！---\n");
-            break;
-        }
-
-        if main_field.all_opened() {
-            println!("\n--- 干得漂亮，已找到所有地雷！---\n");
-            break;
-        }
+        if key == console::Key::Char(' ') {
+            if !main_field.open() {
+                println!("\n--- Oops, 踩雷！---\n");
+                break;
+            }
+            if main_field.all_opened() {
+                println!("\n--- 干得漂亮，已找到所有地雷！---\n");
+                break;
+            }    
+        }        
 
         // 将当前光标向上移动N行。
         print!("\x1b[{}A", main_field.rows);
